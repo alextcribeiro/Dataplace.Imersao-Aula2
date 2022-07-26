c0d20eea769630dc21e582715815f1894476a85f
Alex Ribeiro <alex.ribeiro@dataplace.com.br>
2022-07-25 21:58:11 -0300

    terminado

---
+++ src/Dataplace.Imersao.Core/Application/Orcamentos/Queries/ObterOrcamentoItemQuery.cs
@@ -0,0 +1,15 @@
+﻿using Dataplace.Core.Domain.Query;
+using Dataplace.Imersao.Core.Application.Orcamentos.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dataplace.Imersao.Core.Application.Orcamentos.Queries
+{
+    internal class ObterOrcamentoItemQuery : QueryRefeshItem<OrcamentoItemViewModel>, IQueryRefeshItem<OrcamentoItemViewModel>
+    {
+        public int NumOrcamento { get; set; }
+    }
+}
---
+++ src/Dataplace.Imersao.Core/Application/Orcamentos/Queries/OrcamentoItemQueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Dataplace.Imersao.Core.Application.Orcamentos.ViewModels;
+using dpLibrary05;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dataplace.Imersao.Core.Application.Orcamentos.Queries
+{
+    internal class OrcamentoItemQueryHandler : IRequestHandler<ObterOrcamentoItemQuery, OrcamentoItemViewModel>
+    { 
+    private readonly IDataAccess _dataAccess;
+    private readonly Domain.Orcamentos.Repositories.IOrcamentoRepository _orcamentoRepository;
+
+        public OrcamentoItemQueryHandler(Domain.Orcamentos.Repositories.IOrcamentoRepository orcamentoRepository, IDataAccess dataAccess)
+        {
+            _orcamentoRepository = orcamentoRepository;
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<OrcamentoItemViewModel> Handle(ObterOrcamentoItemQuery query, CancellationToken cancellationToken)
+        {
+            var sql = $@"
+            SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+            SELECT 
+            OrcamentoItem.CdEmpresa,
+            OrcamentoItem.CdFilial,
+            OrcamentoItem.NumOrcamento,
+            OrcamentoItem.Seq,
+            OrcamentoItem.TpRegistro,
+            OrcamentoItem.CdProduto,
+            OrcamentoItem.DsProduto,
+            OrcamentoItem.Quantidade,
+            OrcamentoItem.PrecoTabela,
+            OrcamentoItem.PercAltPreco,
+            OrcamentoItem.PrecoVenda,
+            OrcamentoItem.Total,
+            OrcamentoItem.Status
+            FROM OrcamentoItem
+            /**where**/	
+            ";
+
+            var builder = new SqlBuilder();
+            var selector = builder.AddTemplate(sql);
+
+
+            builder.Where("orcamento.NumOrcamento = @NumOrcamento", new { query.NumOrcamento });
+            var cmd = new CommandDefinition(selector.RawSql, selector.Parameters, flags: CommandFlags.NoCache);
+            return _dataAccess.Connection.QueryFirstOrDefault<OrcamentoItemViewModel>(cmd);
+
+
+        }
+    }
+}
